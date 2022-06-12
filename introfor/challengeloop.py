326eab25824eb1fc9c8b728709a5762c1466923d
Serico14 <Serico14@users.noreply.github.com>
2022-06-12 06:25:17 +0000

    RPG Story

---
+++ introfor/challengeloop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# create a list of strings
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+#NE_animals= farms[0]["agriculture"]
+
+
+
+# create a second list of strings
+#approved_vendors = ["cisco", "juniper", "big_ip"]
+
+# loop across the list called vendors
+for x in NE_animals:
+    print(x)   
+   
+
+
