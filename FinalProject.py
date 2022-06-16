c8a945135f9254b9d707e82ccb908b6a4a95b9ac
Serico14 <Serico14@users.noreply.github.com>
2022-06-16 20:57:28 +0000

    Final Project

--- FinalProject.py
+++ FinalProject.py
@@ -5,9 +5,13 @@ import os
 import pandas as pd
 import requests
 import io
-    
-# Downloading the csv file from your GitHub account
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 
+import matplotlib.pyplot as plt
+
+# Downloading the csv file from your GitHub account
 url = "https://raw.githubusercontent.com/Serico14/mycode/master/Red_Fin_Rental_Data_Set_April%20-%20Red_Fin_Rental_Data_Set_April.csv" 
 download = requests.get(url).content
 
@@ -17,4 +21,26 @@ df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 #Test =  Printing out the first 5 rows of the dataframe
 #print (df.head())
 
+def main(): 
+    R = 4
+    Average_Monthly_Rent = (20, 35, 30, 35)
+    National_Average = (25, 32, 34, 20)
+    ind = np.arange(R)
+    width = 0.45
+    
+    p1 = plt.bar(ind, Average_Monthly_Rent, width)
 
+    p2 = plt.bar(ind, National_Average, width, bottom= Average_Monthly_Rent)
+    
+    plt.ylabel("Average Monthly Rent (dollars)")
+    plt.title("Average Rental Costs by State")
+    plt.xticks(ind, ("Q1", "Q2", "Q3", "Q4"))
+    plt.yticks(np.arange(0, 81, 10))
+    plt.legend((p1[0], p2[0]), ("AMR", "NMR"))
+
+    # display the graph
+    # plt.show() # you can try this on a Python IDE with a GUI if you'd like
+    plt.savefig("/home/student/mycode/FinalProject.png")
+    
+if __name__ == "__main__":
+    main()
