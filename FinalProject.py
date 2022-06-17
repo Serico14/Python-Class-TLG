2e3e0c5c7a152056bec8b457c5e4d8cf348a5d52
Serico14 <Serico14@users.noreply.github.com>
2022-06-17 20:37:29 +0000

    Final Project Updates

--- FinalProject.py
+++ FinalProject.py
@@ -24,7 +24,7 @@ df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 #Unique State Retrieval 
 dataFrame = pd.DataFrame(df)
 Unique_State = (dataFrame["Region"].unique()) #Pulls State Abbreviation
-Unique_State_Count = (len(Unique_State)) #Count for all Inputs in Region column
+Unique_State_Count = (len(Unique_State)-1) #Count for all Inputs in Region column
 
 
 #Average Monthly by State
@@ -40,8 +40,8 @@ National_Rows = (dataFrame.query("Region== 'National'"))  #"Month== 'April_2022'
 def main(): 
     R = Unique_State_Count                                                    #Inputs along x axis
     #Average_Monthly_Rent = (20,30,40,50)                  #Bar 1 Actual - How to properly map this so that (for example:Texas data is populated for Bar                                                                      dedicated to Texas and not some other region? 
-    Average_Monthly_Rent = (20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20) #Test
-    National_Average = (10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10) #Test2
+    Average_Monthly_Rent = (20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20) #Test
+    National_Average = (10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10) #Test2
     
     #National_Average = (10, 10, 10, 10)                      #Bar 2 - Actual
     ind = np.arange(R)                                       #Order of Bars 
@@ -54,7 +54,7 @@ def main():
     plt.ylabel("Average Monthly Rent (dollars)")
     plt.title("Average Rental Costs by State")
    # plt.xticks(ind, ("1","2","3","4"))
-    plt.xticks(ind, ("WI","MO","MN","National","RI","IL","OH","MD","CA","NC","DC","PA","TX","MI","MA","CO","VA","NV","IN","FL","GA","AZ","TN","NY","NJ","WA","OR"))
+    plt.xticks(ind, ("WI","MO","MN","RI","IL","OH","MD","CA","NC","DC","PA","TX","MI","MA","CO","VA","NV","IN","FL","GA","AZ","TN","NY","NJ","WA","OR"))
     plt.yticks(np.arange(0, 81, 10))
     plt.legend((p1[0], p2[0]), ("AMR", "NMR"))
 
